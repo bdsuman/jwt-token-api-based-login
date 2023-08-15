dd974bf9fc3529e3e91fa62059784220084bf9c3
Suman Sen <mesuman@yahoo.com>
2023-08-15 15:59:28 +0600

    web route documentation link added

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
-    return '<h1>This is an Rest API Based CRM Project No UI </h1>';
+    return '<h1>This is an Rest API Based CRM Project No UI </h1><hr><a href="https://documenter.getpostman.com/view/27190127/2s9Xy6pV5v" target="_blank">Click On See Postman Documentation</a>';
 });
