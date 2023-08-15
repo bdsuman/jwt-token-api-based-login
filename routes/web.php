541376599ae045cb6ae68f8e2450756e8088323b
Suman Sen <mesuman@yahoo.com>
2023-08-15 16:19:12 +0600

    web route github link added

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
-    return '<h1>This is an Rest API Based CRM Project No UI </h1><hr><a href="https://documenter.getpostman.com/view/27190127/2s9Xy6pV5v" target="_blank">Click On See Postman Documentation</a>';
+    return '<h1>This is an Rest API Based CRM Project No UI </h1><hr><a href="https://documenter.getpostman.com/view/27190127/2s9Xy6pV5v" target="_blank">Click On See Postman Documentation</a>
+    <br><br><a href="https://github.com/bdsuman/jwt-token-api-based-crm" target="_blank">Click On See Source Code GitHub</a>
+    ';
 });
